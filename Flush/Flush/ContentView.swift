1ea283e53f811f62930a55ac0bd80638e5cafb4d
Kai Chen <remlostime@gmail.com>
2023-12-12 13:35:51 -0800

    Flush App

---
+++ Flush/Flush/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flush
+//
+//  Created by Kai Chen on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
