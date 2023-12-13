b1f55c7e10153e259cad84108cd4067d2d70c6d5
Kai Chen <remlostime@gmail.com>
2023-12-12 16:45:17 -0800

    Card View

---
+++ Flush/FlushWatch Watch App/PlaceholderView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceholderView.swift
+//  FlushWatch Watch App
+//
+//  Created by Kai Chen on 12/12/23.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+        Text("?")
+    }
+}
+
+#Preview {
+    PlaceholderView()
+}
