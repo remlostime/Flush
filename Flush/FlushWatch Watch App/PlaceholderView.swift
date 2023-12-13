9aecb0a1d5afd79855358a3b97d02395bd23736c
Kai Chen <remlostime@gmail.com>
2023-12-12 18:42:58 -0800

    fix lint

--- Flush/FlushWatch Watch App/PlaceholderView.swift
+++ Flush/FlushWatch Watch App/PlaceholderView.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 struct PlaceholderView: View {
-    var body: some View {
-        ZStack {
-            Text("?")
-            
-            RoundedRectangle(cornerSize: CGSize(width: 5, height: 5),
-                             style: .circular)
-            .strokeBorder(.primary, lineWidth: 1)
-        }
+  var body: some View {
+    ZStack {
+      Text("?")
+
+      RoundedRectangle(cornerSize: CGSize(width: 5, height: 5),
+                       style: .circular)
+        .strokeBorder(.primary, lineWidth: 1)
     }
+  }
 }
 
 #Preview {
-    PlaceholderView()
+  PlaceholderView()
 }
