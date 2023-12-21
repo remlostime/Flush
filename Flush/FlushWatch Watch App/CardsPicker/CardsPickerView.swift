adc326f8b6f0ea3e755ed901d459629cc3045061
Kai Chen <remlostime@gmail.com>
2023-12-20 19:04:52 -0800

    Reset Button

--- Flush/FlushWatch Watch App/CardsPicker/CardsPickerView.swift
+++ Flush/FlushWatch Watch App/CardsPicker/CardsPickerView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardsPickerView: View {
+    private let cardHeight: CGFloat = 50
+
     // MARK: Lifecycle
 
     init(board: Binding<Board>) {
@@ -21,72 +23,92 @@ struct CardsPickerView: View {
     @Binding var board: Board
 
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Text("Players")
-                    .font(.footnote)
-                PlayersNumberView(number: viewModel.playersNumber)
-                    .focusable()
-                    .digitalCrownRotation($viewModel.playersNumberDigitalCrown,
-                                          from: Double(Board.MinPlayerNumber),
-                                          through: Double(Board.MaxPlayerNumber),
-                                          by: 1,
-                                          sensitivity: .medium)
-            }
-
-            Spacer()
-
-            Label("Your Cards", systemImage: "lock")
-                .font(.footnote)
-
-            HStack {
-                ForEach(0 ..< viewModel.privateListCards.count, id: \.self) { index in
-                    if let privateCard = viewModel.privateListCards[index] {
-                        CardView(card: privateCard.card)
-                            .onTapGesture {
-                                viewModel.didTapCard(index, cardType: .private)
-                            }
-                            .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
-                                CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
-                            }
-                    } else {
-                        PlaceholderView(iconSize: 16.0)
-                            .onTapGesture {
-                                viewModel.didTapCard(index, cardType: .private)
-                            }
-                            .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
-                                CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
-                            }
-                    }
-                }
-            }
-
-            Spacer()
-
-            Label("Public Cards", systemImage: "lock.open")
-                .font(.footnote)
-            ScrollView(.horizontal) {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 8) {
                 HStack {
-                    ForEach(0 ..< viewModel.publicListCards.count, id: \.self) { index in
-                        if let publicCard = viewModel.publicListCards[index] {
-                            CardView(card: publicCard.card)
+                    Text("Players")
+                        .font(.footnote)
+                    PlayersNumberView(number: viewModel.playersNumber)
+                        .focusable()
+                        .digitalCrownRotation($viewModel.playersNumberDigitalCrown,
+                                              from: Double(Board.MinPlayerNumber),
+                                              through: Double(Board.MaxPlayerNumber),
+                                              by: 1,
+                                              sensitivity: .medium)
+                }
+
+
+                Label("Your Cards", systemImage: "lock")
+                    .font(.footnote)
+
+                HStack {
+                    ForEach(0 ..< viewModel.privateListCards.count, id: \.self) { index in
+                        if let privateCard = viewModel.privateListCards[index] {
+                            CardView(card: privateCard.card)
                                 .onTapGesture {
-                                    viewModel.didTapCard(index, cardType: .public)
+                                    viewModel.didTapCard(index, cardType: .private)
                                 }
                                 .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
                                     CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
                                 }
+                                .frame(height: cardHeight)
                         } else {
                             PlaceholderView(iconSize: 16.0)
                                 .onTapGesture {
-                                    viewModel.didTapCard(index, cardType: .public)
+                                    viewModel.didTapCard(index, cardType: .private)
                                 }
                                 .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
                                     CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
                                 }
+                                .frame(height: cardHeight)
                         }
                     }
                 }
+
+                Label("Public Cards", systemImage: "lock.open")
+                    .font(.footnote)
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(0 ..< viewModel.publicListCards.count, id: \.self) { index in
+                            if let publicCard = viewModel.publicListCards[index] {
+                                CardView(card: publicCard.card)
+                                    .onTapGesture {
+                                        viewModel.didTapCard(index, cardType: .public)
+                                    }
+                                    .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
+                                        CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
+                                    }
+                                    .frame(height: cardHeight)
+                            } else {
+                                PlaceholderView(iconSize: 16.0)
+                                    .onTapGesture {
+                                        viewModel.didTapCard(index, cardType: .public)
+                                    }
+                                    .sheet(isPresented: $viewModel.isSelectedCardViewPresented) {
+                                        CardPickerDetailsView(listCard: $viewModel.currentSelectedCard)
+                                    }
+                                    .frame(height: cardHeight)
+                            }
+                        }
+                    }
+                }
+
+                Button("Calculate") {
+                    board = Board(privateCards: viewModel.privateCards,
+                                  publicCards: viewModel.publicCards,
+                                  playersNumber: viewModel.playersNumber)
+                    viewModel.isResultViewPresented = true
+                }
+                .sheet(isPresented: $viewModel.isResultViewPresented, content: {
+                    ResultView(board: $board)
+                })
+                .background(.red)
+                .clipShape(Capsule())
+
+                Button("Reset") {
+                    viewModel.reset()
+                }
+                .foregroundStyle(.primary)
             }
         }
         .padding()
