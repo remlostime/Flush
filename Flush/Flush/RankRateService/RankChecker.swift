73f7383a86fd5313b877e62259300633f5153336
Kai Chen <remlostime@gmail.com>
2024-02-26 17:30:09 -0800

    Fix test target (#23)
    * Fix test target
    
    * fix something
    
    * remove module
    
    * remove dependency
    
    ---------
    
    Co-authored-by: Kai Chen <ckai@pinterest.com>

--- Flush/Flush/RankRateService/RankChecker.swift
+++ Flush/Flush/RankRateService/RankChecker.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import FlushModel
 
 // MARK: - RankChecker
 
--- Flush/FlushWatch Watch App/RankResult/ResultView.swift
+++ Flush/FlushWatch Watch App/RankResult/ResultView.swift
@@ -6,8 +6,6 @@
 //
 
 import SwiftUI
-import FlushUI
-import FlushModel
 
 struct ResultView: View {
     // MARK: Lifecycle
